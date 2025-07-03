1750c1fdad3e26e06c21dc41236035139dbe6433
Dima Pristupa <dima@pristupa.dev>
2025-07-03 12:32:11 +0300

    App: introduce

---
+++ src/app/bridge/src/app.rs
@@ -0,0 +1,9 @@
+pub struct App {}
+
+impl App {
+    pub async fn run(&self) -> Result<(), String> {
+        // todo
+        async {}.await;
+        Ok(())
+    }
+}
--- src/app/bridge/src/main.rs
+++ src/app/bridge/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use kamu_molecule_bridge::prelude::*;
+
+fn main() -> ExitCode {
+    // TODO: rustls initialize stuff
+    // TODO: read config
+    // TODO: logging
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(main_async())
+}
+
+async fn main_async() -> ExitCode {
+    let app = App {};
+
+    match app.run().await {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            tracing::error!(error = %e, error_dbg = ?e, "Error running application");
+            ExitCode::FAILURE
+        }
+    }
 }
