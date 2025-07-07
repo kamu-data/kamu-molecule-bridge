df54790820f2d5f926670ddb6f7e497bbdf8a430
Dima Pristupa <dima@pristupa.dev>
2025-07-07 20:29:57 +0300

    Load config from env

--- src/app/bridge/src/config.rs
+++ src/app/bridge/src/config.rs
@@ -1,9 +1,23 @@
 use alloy::primitives::Address;
 
+#[derive(confique::Config, Debug)]
 pub struct Config {
-    pub provider_url: String,
+    #[config(env = "RPC_URL")]
+    pub rpc_url: String,
+
+    #[config(env = "IPNFT_CONTRACT_ADDRESS")]
     pub ipnft_contract_address: Address,
+    #[config(env = "IPNFT_CONTRACT_BIRTH_BLOCK")]
     pub ipnft_contract_birth_block: u64,
+
+    #[config(env = "TOKENIZER_CONTRACT_ADDRESS")]
     pub tokenizer_contract_address: Address,
+    #[config(env = "TOKENIZER_CONTRACT_BIRTH_BLOCK")]
     pub tokenizer_contract_birth_block: u64,
+}
+
+impl Config {
+    pub fn builder() -> confique::Builder<Config> {
+        confique::Config::builder()
+    }
 }
--- src/app/bridge/src/main.rs
+++ src/app/bridge/src/main.rs
@@ -1,28 +1,47 @@
-use std::process::ExitCode;
-
+use alloy::providers::fillers::ChainIdFiller;
+use alloy::providers::{DynProvider, Provider};
+use color_eyre::eyre;
+use dotenv::dotenv;
 use kamu_molecule_bridge::prelude::*;
 
-fn main() -> ExitCode {
-    // TODO: rustls initialize stuff
-    // TODO: read config
-    // TODO: logging
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
+    // TODO: Tracing initialization
+    dotenv()?;
+
+    let config = Config::builder()
+        .env()
+        // TODO: Add support for config file
+        // .file(&args.config)
+        .load()?;
 
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .build()
-        .unwrap();
+        .build()?;
 
-    rt.block_on(main_async())
+    rt.block_on(main_async(config))
 }
 
-async fn main_async() -> ExitCode {
-    let app = App {};
+async fn main_async(config: Config) -> eyre::Result<()> {
+    let provider = build_rpc_client(&config).await?;
+    let app = App::new(config, provider);
 
-    match app.run().await {
-        Ok(_) => ExitCode::SUCCESS,
-        Err(e) => {
-            tracing::error!(error = %e, error_dbg = ?e, "Error running application");
-            ExitCode::FAILURE
-        }
-    }
+    app.run().await?;
+
+    Ok(())
+}
+
+// TODO: move?
+async fn build_rpc_client(config: &Config) -> eyre::Result<DynProvider> {
+    let provider = alloy::providers::ProviderBuilder::new()
+        // We do not work with transactions, so we disable all filters ...
+        .disable_recommended_fillers()
+        // ... except caching filter for ChainId.
+        .filler(ChainIdFiller::default())
+        .connect(&config.rpc_url)
+        .await?
+        .erased();
+
+    Ok(provider)
 }
