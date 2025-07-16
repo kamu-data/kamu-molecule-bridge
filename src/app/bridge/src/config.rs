31a374075dfba2b27f244ff48f547fd67cd97928
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2025-07-16 10:36:57 -0700

    Chore: Healthchecks and metrics (#5)
    * chore: HTTP server with healthchecks
    
    * chore: Prometheus metrics

--- src/app/bridge/src/config.rs
+++ src/app/bridge/src/config.rs
@@ -4,25 +4,40 @@ use alloy::primitives::Address;
 //       - use https://github.com/Keats/validator for field validation
 #[derive(confique::Config, Debug)]
 pub struct Config {
-    #[config(env = "KAMU_NODE_GQL_API_ENDPOINT")]
+    /// Interface to listen for HTTP admin traffic on
+    #[config(env = "KAMU_BRIDGE_HTTP_ADDRESS")]
+    #[config(default = "127.0.0.1")]
+    pub http_address: std::net::IpAddr,
+
+    /// Port to listen for HTTP admin traffic on
+    #[config(env = "KAMU_BRIDGE_HTTP_PORT")]
+    #[config(default = 0)]
+    pub http_port: u16,
+
+    #[config(env = "KAMU_BRIDGE_KAMU_NODE_GQL_API_ENDPOINT")]
     pub kamu_node_gql_api_endpoint: String,
-    #[config(env = "KAMU_NODE_TOKEN")]
+    #[config(env = "KAMU_BRIDGE_KAMU_NODE_TOKEN")]
     pub kamu_node_token: String,
 
-    #[config(env = "MOLECULE_PROJECTS_DATASET_ALIAS")]
+    #[config(env = "KAMU_BRIDGE_MOLECULE_PROJECTS_DATASET_ALIAS")]
     pub molecule_projects_dataset_alias: String,
 
-    #[config(env = "RPC_URL")]
+    /// ID of the chain that RCP URL is expected to point to
+    #[config(env = "KAMU_BRIDGE_CHAIN_ID")]
+    #[config(default = 0)]
+    pub chain_id: u64,
+
+    #[config(env = "KAMU_BRIDGE_RPC_URL")]
     pub rpc_url: String,
 
-    #[config(env = "IPNFT_CONTRACT_ADDRESS")]
+    #[config(env = "KAMU_BRIDGE_IPNFT_CONTRACT_ADDRESS")]
     pub ipnft_contract_address: Address,
-    #[config(env = "IPNFT_CONTRACT_BIRTH_BLOCK")]
+    #[config(env = "KAMU_BRIDGE_IPNFT_CONTRACT_BIRTH_BLOCK")]
     pub ipnft_contract_birth_block: u64,
 
-    #[config(env = "TOKENIZER_CONTRACT_ADDRESS")]
+    #[config(env = "KAMU_BRIDGE_TOKENIZER_CONTRACT_ADDRESS")]
     pub tokenizer_contract_address: Address,
-    #[config(env = "TOKENIZER_CONTRACT_BIRTH_BLOCK")]
+    #[config(env = "KAMU_BRIDGE_TOKENIZER_CONTRACT_BIRTH_BLOCK")]
     pub tokenizer_contract_birth_block: u64,
 }
 
--- src/app/bridge/src/lib.rs
+++ src/app/bridge/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod app;
 pub mod config;
+pub mod http_server;
+pub mod metrics;
 pub mod prelude;
