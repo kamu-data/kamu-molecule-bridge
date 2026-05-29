66d0b9662c74c4181d6c73f35b15a9c7fc640017
Dima Pristupa <dima@pristupa.dev>
2026-05-29 17:27:40 +0300

    OclOwnershipChange: add a NOTE re no burn function

--- src/domain/molecule_ocl/src/entities/ocl_ownership_change.rs
+++ src/domain/molecule_ocl/src/entities/ocl_ownership_change.rs
@@ -7,6 +7,8 @@ use crate::entities::OclId;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct OclOwnershipChange {
     pub former_owner: Option<Address>,
+    // Note: no burn function, so no need for Option<Address>
+    // https://github.com/moleculeprotocol/onchainlabs/blob/c69b3774a887906a3a05983d4a410847a189a779/docs/nft/labnft-solady-migration-plan.md?plain=1#L704
     pub current_owner: Address,
 }
 
